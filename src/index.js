027ae96e0cbe1d3ce13717448483e8e86649fea5
eyucherin <yu24c@mtholyoke.edu>
2023-10-10 20:01:55 -0400

    higher order component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import FormApp from "./FormApp";
+import HOCApp from "./HOCApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FormApp />
+    <HOCApp />
   </React.StrictMode>
 );
 
